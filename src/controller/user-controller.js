9c0b942b2c53c661d3ca3676d9e8f90919d05738
conicuznhm <conicuzn@hotmail.com>
2023-03-02 18:19:15 +0700

    begin set user edit

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -1,5 +1,6 @@
 const { User } = require("../models");
 const createError = require("../util/createError");
+const { validateCreateProfile } = require("../validator/profile-validate");
 
 exports.getAllUser = async (req, res, next) => {
   try {
@@ -21,6 +22,54 @@ exports.getUserById = async (req, res, next) => {
       createError("You not have permission to access this user", 400);
     }
     res.status(200).json({ user });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.createProfile = async (req, res, next) => {
+  try {
+    const value = validateCreateProfile({
+      name: req.body.name,
+      image: req.files?.image[0].path,
+      coverImage: req.files?.coverImage[0].path,
+    });
+
+    value.userId = req.user.id;
+
+    const user = await User.create({
+      name: value.name,
+      image: value.image,
+      coverImage: value.coverImage,
+      userId: value.userId,
+    });
+    res.status(201).json({ User });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.updateProfile = async (req, res, next) => {
+  try {
+    const value = validateCreateProfile({
+      name: req.body.name,
+      image: req.files?.image[0].path,
+      coverImage: req.files?.coverImage[0].path,
+    });
+
+    const result = await User.update(
+      {
+        name: value.name,
+        image: value.image,
+        coverImage: value.coverImage,
+      },
+      {
+        where: {
+          id: req.params.profileId,
+        },
+      },
+    );
+    res.status(200).json({ result });
   } catch (err) {
     next(err);
   }
--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controller/user-controller");
+const upload = require("../middlewares/upload");
 
 router.get("/", userController.getAllUser);
 router.get("/:userId", userController.getUserById);
+
+router.post(
+  "/",
+  upload.fields([
+    { name: "image", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  userController.createProfile,
+);
+
+router.patch(
+  "/:userId",
+  upload.fields([
+    { name: "image", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  userController.updateProfile,
+);
 
 module.exports = router;
